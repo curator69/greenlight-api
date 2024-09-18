9426f675e6ccdc5636a594f81c03d33cc4c2acbb
Rushikesh <rushikeshkhillari10@gmail.com>
2024-09-18 16:00:32 +0530

    added logic to handle race conditions using optimistic concurrency control

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -42,9 +42,9 @@ func ValidateMovie(v *validator.Validator, movie *Movie) {
 
 func (m MovieModel) Insert(movie *Movie) error {
 	query := `
-INSERT INTO movies (title, year, runtime, genres)
-VALUES ($1, $2, $3, $4)
-RETURNING id, created_at, version`
+		INSERT INTO movies (title, year, runtime, genres)
+		VALUES ($1, $2, $3, $4)
+		RETURNING id, created_at, version`
 
 	args := []interface{}{movie.Title, movie.Year, movie.Runtime, pq.Array(movie.Genres)}
 
@@ -90,7 +90,7 @@ func (m MovieModel) Update(movie *Movie) error {
 	query := `
 		UPDATE movies
 		SET title = $1, year = $2, runtime = $3, genres = $4, version = version + 1
-		WHERE id = $5
+		WHERE id = $5 AND version = $6
 		RETURNING version`
 
 	args := []interface{}{
@@ -99,9 +99,20 @@ func (m MovieModel) Update(movie *Movie) error {
 		movie.Runtime,
 		pq.Array(movie.Genres),
 		movie.ID,
+		movie.Version,
 	}
 
-	return m.DB.QueryRow(query, args...).Scan(&movie.Version)
+	err := m.DB.QueryRow(query, args...).Scan(&movie.Version)
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return ErrEditConflict
+		default:
+			return err
+		}
+	}
+
+	return nil
 }
 
 func (m MovieModel) Delete(id int64) error {
