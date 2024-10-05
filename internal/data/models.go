9cdeebbc3342f39763e876c0ae79ec76b5ed8f56
rushikesh <rushikeshkhillari10@gmail.com>
2024-10-05 20:42:29 +0530

    added the add permission model

--- internal/data/models.go
+++ internal/data/models.go
@@ -12,6 +12,7 @@ var (
 
 type Models struct {
 	Movies MovieModel
+	Permissions PermissionModel
 	Tokens TokenModel
 	Users  UserModel
 }
@@ -19,6 +20,7 @@ type Models struct {
 func NewModels(db *sql.DB) Models {
 	return Models{
 		Movies: MovieModel{DB: db},
+		Permissions: PermissionModel{DB: db},
 		Tokens: TokenModel{DB: db},
 		Users:  UserModel{DB: db},
 	}
